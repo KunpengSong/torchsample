8cfed4e85d9a69669a0ee01bcd0c34560e2ec0bd
Achaiah <achaiah@gmail.com>
2018-09-17 12:50:23 -0500

    small bugfix

--- models/segmentation/testnets/__init__.py
+++ models/segmentation/testnets/__init__.py
@@ -4,7 +4,6 @@ from .deeplabv2_speeding import Res_Ms_Deeplab as TEST_Ms_DLV2
 from .deeplabv3_xception import create_DLX_V3_pretrained
 from .deeplabv3_resnet import create_DLR_V3_pretrained
 from .dilated_linknet import DilatedLinkNet34 as TEST_DiLinknet
-from .icnet import icnet as TEST_icnet
 from .linknext import LinkNext as TEST_Linknext
 from .standard_fc_densenets import FCDenseNet103 as TEST_FCDensenet
 from .psp_saeed import PSPNet as TEST_PSPNet2
